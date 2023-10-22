76a62ccb92bd41836a41c0bf98dd88eedf42f586
Steven Lam <34800247+StvnLm@users.noreply.github.com>
2023-10-22 15:58:50 -0400

    remove unused tests (#10)
    Co-authored-by: StvnLm <>

--- test/patient_test.cpp
+++ test/patient_test.cpp
@@ -3,19 +3,6 @@
 #include "../src/patient.cpp"
 #include <string>
 
-// Parameters
-// PatientTest: Name of the test Suite
-// GetName: Name of the unit test
-TEST(PatientTest, GetName) {
-	Patient patient("John", 30, 'M');
-}
-
-TEST(PatientTest, SetAge) {
-    Patient patient("Alice", 25, 'F');
-    EXPECT_TRUE(patient.set_age(26));
-    EXPECT_EQ(patient.get_age(), 26);
-}
-
 TEST(PatientTest, setPatientValues) {
 	Patient patient;
 	EXPECT_TRUE(patient.set_name("Jane Doe"));
