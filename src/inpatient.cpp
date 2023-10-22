4e4d10cdef82fba7b2abcc371cb9313ebfb4300c
Steven Lam <34800247+StvnLm@users.noreply.github.com>
2023-10-22 15:59:28 -0400

    Develop (#11)
    * add inpatient implementation functions (#8)
    
    Co-authored-by: StvnLm <>
    
    * Patient test (#9)
    
    * add additional tests for patient
    
    * add get_location, fixed tests for correct returns
    
    ---------
    
    Co-authored-by: StvnLm <>
    
    * remove unused tests (#10)
    
    Co-authored-by: StvnLm <>

--- src/inpatient.cpp
+++ src/inpatient.cpp
@@ -5,7 +5,23 @@ using namespace std;
 Inpatient::Inpatient() : room_number(0), services_required() {
 }
 
-Inpatient::Inpatient(int room_number, list<Patient_services> services_required) : {
-	this->room_number = room_number;
-	this->services_required = services_required;
+Inpatient::Inpatient(int rn, list<Patient_services> sr) : room_number(rn), services_required(sr) {
+}
+
+bool Inpatient::set_room_number(int n) {
+	room_number = n;
+	return room_number > 0;
+}
+
+bool Inpatient::set_services_required(list<Patient_services> n) {
+	services_required = n;
+	return !services_required.empty();
+}
+
+int Inpatient::get_room_number() {
+	return room_number;
+};
+
+list<Patient_services> Inpatient::get_services_required() {
+	return services_required;
 }
--- test/patient_test.cpp
+++ test/patient_test.cpp
@@ -3,15 +3,16 @@
 #include "../src/patient.cpp"
 #include <string>
 
-// Parameters
-// PatientTest: Name of the test Suite
-// GetName: Name of the unit test
-TEST(PatientTest, GetName) {
-	Patient patient("John", 30, 'M');
-}
-
-TEST(PatientTest, SetAge) {
-    Patient patient("Alice", 25, 'F');
-    EXPECT_TRUE(patient.set_age(26));
-    EXPECT_EQ(patient.get_age(), 26);
+TEST(PatientTest, setPatientValues) {
+	Patient patient;
+	EXPECT_TRUE(patient.set_name("Jane Doe"));
+	EXPECT_TRUE(patient.set_age(100));
+	EXPECT_TRUE(patient.set_gender('F'));
+	EXPECT_TRUE(patient.set_location("100 Bay street, Toronto, Ontario, L5X 2A8"));
+	EXPECT_TRUE(patient.set_ailment("Broken bones"));
+	EXPECT_EQ(patient.get_name(), "Jane Doe");
+	EXPECT_EQ(patient.get_age(), 100);
+	EXPECT_EQ(patient.get_gender(), 'F');
+	EXPECT_EQ(patient.get_location(), "100 Bay street, Toronto, Ontario, L5X 2A8");
+	EXPECT_EQ(patient.get_ailment(), "Broken bones");
 }
