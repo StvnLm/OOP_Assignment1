8206530dc3ca57f55d5ebdba0c2927db82fdcfe2
HugoLin0430 <115123530+HugoLin0430@users.noreply.github.com>
2023-11-23 10:40:42 -0500

    Patient class files location
    update files location

--- src/patient.cpp
+++ src/patient.cpp
@@ -1,63 +1,30 @@
-#include "../header/patient.h"
-#include <tuple>
-#include "../header/invalidAgeException.h"
+// Created by Jianfeng Lin
 
-using namespace std;
+#include "Patient.h"
+#include <iostream>
 
-Patient::Patient() : name("Jane Doe"),  age(0),  gender('N'), ailment("NONE") {
+Patient::Patient(const std::string& _name, int _age)
+    : name(_name), age(_age) {}
+
+Patient::~Patient() {}
+
+void Patient::setName(const std::string& _name) {
+    name = _name;
 }
 
-Patient::Patient(string n, int a, char g, string ai) : name(n), age(a), gender(g), ailment(ai) {
+std::string Patient::getName() const {
+    return name;
 }
 
-tuple<string, char, int> Patient::get_patient_info() {
-	return make_tuple(name, gender, age);
+void Patient::setAge(int _age) {
+    age = _age;
 }
 
-bool Patient::set_name(string n) {
-	name = n;
-	return name.size() > 0;
+int Patient::getAge() const {
+    return age;
 }
 
-bool Patient::set_gender(char g) {
-	gender = g;
-	return (tolower(gender) != 'm' || tolower(gender) != 'f');
-}
-
-bool Patient::set_age(int a) {
-	if (a < 0) {
-		throw InvalidAgeException("Age cannot be negative");
-	}
-	age = a;
-	return age > 0;
-}
-
-bool Patient::set_location(string l) {
-	location = l; 
-	return location != "";
-}
-
-bool Patient::set_ailment(string a) {
-	ailment = a;
-	return ailment != "";
-}
-
-string Patient::get_name() {
-	return name;
-}
-
-char Patient::get_gender() {
-	return gender;
-}
-
-int Patient::get_age() {
-	return age;
-}
-
-string Patient::get_ailment() {
-	return ailment;
-}
-
-string Patient::get_location() {
-	return location;
+void Patient::displayInfo() const {
+    std::cout << "Name: " << name << "\n";
+    std::cout << "Age: " << age << "\n";
 }
