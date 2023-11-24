7e320c9862103a93c519f6b55c20dc5e0a156ac9
HugoLin0430 <115123530+HugoLin0430@users.noreply.github.com>
2023-11-24 10:56:57 -0500

    Develop (#32)
    * Patient class files location
    
    update files location
    
    * Finish Edition

--- header/appointment.h
+++ header/appointment.h
@@ -4,27 +4,64 @@
 #include "./inpatient.h"
 #include "./med_staff.h"
 #include <string>
+#include <memory>  
+#include <vector>
 
 class Appointment {
-  public:
+public:
     Appointment();
-    int calculateIndex(int day, int month, int year);
     void addAppointment(int day, int month, int year, int hour, int min, Patient& patient, MedicalStaff& staff);
     void removeAppointment(int day, int month, int year, int hour, int min, Patient& patient, MedicalStaff& staff);
-    void printAppointments();
-  private:
-    static const int appointmentTableSize = 365;
-    struct appointment {
-      int month;
-      int day;
-      int year;
-      int hour;
-      int min;
-      Patient* patient;
-      MedicalStaff* medicalStaff;
-      appointment* next;      
+    void printAppointments() const;
+
+// New methods to get appointment details
+    std::string getPatientName(int index) const {
+        if (index >= 0 && index < appointments.size()) {
+            return appointments[index]->patient->getName();
+        }
+        return "";
+    }
+
+    std::string getStaffName(int index) const {
+        if (index >= 0 && index < appointments.size()) {
+            return appointments[index]->medicalStaff->getName();
+        }
+        return "";
+    }
+
+    std::string getDate(int index) const {
+        if (index >= 0 && index < appointments.size()) {
+            return std::to_string(appointments[index]->day) + "/" +
+                   std::to_string(appointments[index]->month) + "/" +
+                   std::to_string(appointments[index]->year);
+        }
+        return "";
+    }
+
+    std::string getTime(int index) const {
+        if (index >= 0 && index < appointments.size()) {
+            return std::to_string(appointments[index]->hour) + ":" +
+                   std::to_string(appointments[index]->min);
+        }
+        return "";
+    }
+
+    int getNumberOfAppointments() const {
+        return appointments.size();
+    }
+
+
+private:
+    struct AppointmentDetails {
+        int day, month, year, hour, min;
+        Patient* patient;
+        MedicalStaff* medicalStaff;
     };
-    appointment* appointmentTable[appointmentTableSize];
+
+    std::vector<std::unique_ptr<AppointmentDetails>> appointments;
+
+    bool isValidDate(int day, int month, int year) const;
+    bool isValidTime(int hour, int min) const;
 };
 
-#endif
+#endif // APPOINTMENT_H
